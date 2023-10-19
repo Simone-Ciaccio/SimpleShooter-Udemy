6ac84786d0a827ce93cebbe99a5e54dfce152133
Simone-Ciaccio <cimocs@hotmail.it>
2023-10-19 17:47:02 +0200

    Finished video "Calculating the win condition"

--- Source/SimpleShooter/KillEmAllGameMode.cpp
+++ Source/SimpleShooter/KillEmAllGameMode.cpp
@@ -5,6 +5,8 @@
 
 #include "EngineUtils.h"
 
+#include "ShooterAIController.h"
+
 void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 {
 	Super::PawnKilled(PawnKilled);
@@ -14,6 +16,16 @@ void AKillEmAllGameMode::PawnKilled(APawn* PawnKilled)
 	{
 		EndGame(false);
 	}
+
+	for (AShooterAIController* AIController : TActorRange<AShooterAIController>(GetWorld()))
+	{
+		if (!AIController->IsDead())
+		{
+			return;
+		}
+	}
+
+	EndGame(true);
 }
 
 void AKillEmAllGameMode::EndGame(bool bIsPlayerWinner)
